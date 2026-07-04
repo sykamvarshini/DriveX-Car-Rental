a6532a568ba4360e7b6dbca8ab01e8ffb32f0435
sykamvarshini <sykamvarshini30@gmail.com>
2026-07-04 16:44:24 +0530

    Updated server for Render deployment

--- server/server.js
+++ server/server.js
@@ -23,6 +23,8 @@ app.get('/', (req, res) => {
   res.send('DriveX API Running');
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
